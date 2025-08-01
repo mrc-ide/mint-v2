fdd964c5dc7c703a6e4f71f70bc4880abae14fe2
anmol thapar <mr.anmolthapar@gmail.com>
2025-08-01 14:03:48 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import type { PageProps } from "./$types";
+    let { data }: PageProps = $props();
+
+    $inspect(data, "data");
+</script>
+
+<h1>Welcome to {data.name}</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
