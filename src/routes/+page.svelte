307d51a93da9168777d8b3b1d5ec87918aed842d
anmol thapar <mr.anmolthapar@gmail.com>
2025-08-01 14:50:13 +0100

    setup shadcn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
+	import { Button } from "$lib/components/ui/button";
     import type { PageProps } from "./$types";
+    
     let { data }: PageProps = $props();
 
-    $inspect(data, "data");
+    $inspect(data);
 </script>
 
 <h1>Welcome to {data.name}</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Button>Click me daddy</Button>
