442c335571c271ecd58d1bfd07d3123c9983cfc3
anmol thapar <mr.anmolthapar@gmail.com>
2025-09-04 19:40:10 +0100

    fix: revert API_VERSION to main for consistency after merging

--- scripts/common.sh
+++ scripts/common.sh
@@ -3,7 +3,7 @@
 REGISTRY=ghcr.io
 ORG=mrc-ide
 API=mintr
-API_VERSION=mrc-6656-model-runs # TODO: revert to main after merging R 
+API_VERSION=main # TODO: revert to main after merging R 
 NETWORK=mint_network
 NAME_REDIS=mint-redis
 
