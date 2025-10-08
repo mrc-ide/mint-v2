673b0ea354d784fbcda29c4d947bca021065b216
anmol thapar <mr.anmolthapar@gmail.com>
2025-10-08 14:10:04 +0100

    fix: update Playwright configuration to include parallel testing and CI settings

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,5 +5,10 @@ export default defineConfig({
 		command: 'npm run build && npm run preview',
 		port: 4173
 	},
-	testDir: 'e2e'
+	testDir: 'e2e',
+	fullyParallel: true,
+	forbidOnly: !!process.env.CI,
+	retries: process.env.CI ? 2 : 0,
+	reporter: process.env.CI ? 'github' : 'list',
+	workers: process.env.CI ? 1 : undefined
 });
