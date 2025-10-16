edd3ddee89153f9449ee957531f2183e674e29d7
anmol thapar <mr.anmolthapar@gmail.com>
2025-10-16 16:04:27 +0100

    fix: reset user state if parsing cached state fails

--- src/lib/server/redis.ts
+++ src/lib/server/redis.ts
@@ -18,16 +18,18 @@ export default redis;
 
 export const loadOrSetupUserState = async (cookies: Cookies, event: RequestEvent): Promise<UserState> => {
 	const userId = cookies.get('userId') || '';
-
 	const cachedUserState = await redis.get(userId);
 	if (cachedUserState) {
 		try {
 			return JSON.parse(cachedUserState);
 		} catch (error) {
 			console.error('Error parsing user state from Redis:', error);
+			// If parsing fails, reset the user state with new User ID
+			console.warn('Resetting user state due to parsing error');
 		}
 	}
 
+	// If no cached user state exists or error occurs, create a new one
 	return createAndPersistNewUserState(cookies, event);
 };
 
