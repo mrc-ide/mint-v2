cf771c19e26a5576aa7c074951a7dc4501f068ef
anmol thapar <mr.anmolthapar@gmail.com>
2026-01-15 15:30:13 +0000

    get working version of endpoints

--- server-python/app/main.py
+++ server-python/app/main.py
@@ -3,9 +3,11 @@ from minte import __version__ as minte_version
 
 from app import __version__
 
-from .models import VersionResponse
+from .models import EmulatorRequest, EmulatorResponse, VersionResponse
+from .services.emulator import run_emulator_model
+from .services.resources import get_dynamic_form_options
 
-app = FastAPI(title="My API", version=__version__)
+app = FastAPI(title="MINT API", version=__version__)
 
 
 @app.get("/version")
@@ -16,3 +18,13 @@ async def get_version() -> VersionResponse:
 @app.get("/healthz")
 async def health_check():
     return {"status": "ok"}
+
+
+@app.get("/dynamic-form-options")
+async def dynamic_form_options():
+    return get_dynamic_form_options()
+
+
+@app.post("/emulator/run")
+async def run_emulator(emulator_request: EmulatorRequest) -> EmulatorResponse:
+    return run_emulator_model(emulator_request)
