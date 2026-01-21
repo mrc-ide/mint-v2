e4625c0710ee36b2c52815f67386a2c793455903
anmol thapar <mr.anmolthapar@gmail.com>
2026-01-21 11:02:21 +0000

    Improve error handling in apiFetch and update related tests for clarity

--- client/src/lib/fetch.ts
+++ client/src/lib/fetch.ts
@@ -39,7 +39,16 @@ export const apiFetch = async <T>({ url, method = 'GET', body, fetcher = fetch }
 		throw new ApiError(`Request failed with status ${res.status}`, res.status);
 	}
 
-	console.error(data);
-	const errorMessage = (data as ResponseBodyFailure).detail || 'Unknown error';
+	console.error('API Fetch Error:', data);
+	let errorMessage = 'Unknown error occurred';
+	// App.Error type from sveltekit
+	if ('message' in data) {
+		errorMessage = data.message;
+	}
+	// ResponseBodyFailure type
+	if ('detail' in data) {
+		errorMessage = data.detail;
+	}
+
 	throw new ApiError(errorMessage, res.status);
 };
