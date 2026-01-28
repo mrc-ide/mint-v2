e1f83fd228803cf7d3874491e34e25aff98eee4d
anmol thapar <mr.anmolthapar@gmail.com>
2026-01-28 15:22:55 +0000

    fix: update API_REF to match current branch for consistency

--- client/scripts/common.sh
+++ client/scripts/common.sh
@@ -3,7 +3,7 @@
 REGISTRY=ghcr.io
 ORG=mrc-ide
 NAME_API=mint-api
-API_REF=r-python-endpoints # TODO: revert back to main before me
+API_REF=mrc-6840-compare-prev-plot# TODO: revert back to main before me
 NETWORK=mint_network
 NAME_REDIS=mint-redis
 
